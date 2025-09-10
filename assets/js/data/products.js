fc5df296f588c1220a66e779125c83692f855c38
OSX-L1 <mrposx101@gmail.com>
2025-09-11 01:01:03 +0700

    Add files via upload

---
+++ assets/js/data/products.js
@@ -0,0 +1,9 @@
+// assets/js/data/products.js
+// ตั้งค่า Google Sheets สำหรับ "มู่ลี่อลูมิเนียม"
+// sheet1 = ราคาธรรมดา, sheet2 = ราคาโซ่วน (ตามที่ผู้ใช้ให้)
+window.appConfig = window.appConfig || {};
+window.appConfig.sheets = window.appConfig.sheets || {};
+window.appConfig.sheets.alu = {
+  apiV4Normal: { spreadsheetId: "1l_2uDKEY8MCp7B0yrzGlg-DyiPvej7gnu0vQIoykmbg", range: "sheet1!A:Z", apiKey: "AIzaSyCiqmvbS4nSA_P7HPds4Hf5PpzkWGCTTIY" },
+  apiV4Zo:     { spreadsheetId: "1l_2uDKEY8MCp7B0yrzGlg-DyiPvej7gnu0vQIoykmbg", range: "sheet2!A:Z", apiKey: "AIzaSyCiqmvbS4nSA_P7HPds4Hf5PpzkWGCTTIY" }
+};
