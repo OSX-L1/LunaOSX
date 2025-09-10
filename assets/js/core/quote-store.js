c7c8f4271a93cccfc60e8535b7392ea7b2a28ecf
OSX-L1 <mrposx101@gmail.com>
2025-09-11 00:56:38 +0700

    Delete assets/js/core directory

--- assets/js/core/quote-store.js
+++
@@ -1,8 +0,0 @@
-// assets/js/core/quote-store.js
-(function(){
-  const store = [];
-  function add(items){ if(Array.isArray(items)) store.push(...items); }
-  function list(){ return [...store]; }
-  function clear(){ store.length = 0; }
-  window.CombinedQuote = { add, list, clear };
-})();
